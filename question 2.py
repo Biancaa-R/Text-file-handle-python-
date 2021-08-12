e033abdc56d7c1b8660c96c273f8cd4018af674e
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2021-08-12 11:22:38 +0530

    Update question 2.py

--- question 2.py
+++ question 2.py
@@ -2,7 +2,7 @@
  from a text file STORY.TXT,
 and display those words, which are less than 4 characters. """
 
-F=open("D:\\12th file handle\\story.txt","r")
+F=open("story.txt","r")
 value=F.read()
 lines=value.split()
 count=0
