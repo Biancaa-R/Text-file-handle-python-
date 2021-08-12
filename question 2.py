3677af8fcbb55d3b1f3b5d885e17ae72582b25c6
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2021-08-12 22:37:53 +0530

    Update question 2.py

--- question 2.py
+++ question 2.py
@@ -1,5 +1,6 @@
 """ Write a method/function DISPLAYWORDS() in python to read lines
  from a text file STORY.TXT,
+ using read function
 and display those words, which are less than 4 characters. """
 
 F=open("story.txt","r")
