a1fab50c98968ad8a75582e06e863bbd3ddc76be
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2021-08-15 09:18:37 +0530

    Add files via upload

---
+++ question 6.py
@@ -0,0 +1,33 @@
+'''Write a function countmy( )in Python to read the text file
+“Story.txt” and count the number of times “my” or “My” occurs in
+the file.:'''
+
+#Try using both read and readlines
+
+def count_my(): #using read
+    F=open("D:\\12th file handle\\hi.txt","r")
+    count=0
+    value=F.read()
+    line=value.split()
+
+    for i in line:
+        if i in ["my","My","MY"]:
+            count+=1
+    print("The total occurances of my is",count)
+
+
+count_my()
+
+def count_my2(): #using readlines
+    F=open("D:\\12th file handle\\hi.txt","r")
+    count=0
+    value=F.readlines()
+    for line in value:
+        line=line.split()
+        for i in line:
+            if i in ["my","My","MY"]:
+                count+=1
+    print("The total occurances of my is",count)
+
+
+count_my2()
